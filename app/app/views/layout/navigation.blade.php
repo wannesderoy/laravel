ef5b7707a841068b90c38b423abfd564184c0c57
wannesderoy <wannesderoy@gmail.com>
2014-03-30 19:57:24 +0200

    fine tuned activation request

--- app/app/views/layout/navigation.blade.php
+++ app/app/views/layout/navigation.blade.php
@@ -2,7 +2,7 @@
 	<ul>
 		<li><a href="{{ URL::route('home') }}">Home</a></li>
 		@if(Auth::check())
-			you are signed in (navigation.blad.php)
+			you are signed in (navigation.blade.php)
 		@else
 			<li><a href="{{ URL::route('account-sign-in') }}">Sign In</a></li>
 			<li><a href="{{ URL::route('account-create') }}">Create Account</a></li>
